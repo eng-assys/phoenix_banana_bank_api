909a3b91b75340ff71ef47c43d8202c924eae2cb
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-05 17:03:20 -0300

    add user facade

--- lib/banana_bank_web/controllers/users_controller.ex
+++ lib/banana_bank_web/controllers/users_controller.ex
@@ -1,23 +1,16 @@
 defmodule BananaBankWeb.UsersController do
   use BananaBankWeb, :controller
 
-  alias BananaBank.Users.Create
-  alias BananaBank.Users.User
+  alias BananaBank.Users
+  alias Users.User
 
   action_fallback BananaBankWeb.FallbackController
 
   def create(conn, params) do
-    with {:ok, %User{} = user} <- Create.call(params) do
+    with {:ok, %User{} = user} <- Users.create(params) do
       conn
         |> put_status(:created)
         |> render("create.json", user: user)
     end
   end
-
-  # defp handle_response({:error, changeset}, conn) do
-  #   conn
-  #   |> put_status(:bad_request)
-  #   |> put_view(json: BananaBankWeb.ErrorJSON)
-  #   |> render(:error, changeset: changeset)
-  # end
 end
