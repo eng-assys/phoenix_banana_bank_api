74c8222e4c56425e74b6baa3c25e89787d2a5fce
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-05 16:48:03 -0300

    add fallback controller; use standard name for controllers

---
+++ lib/banana_bank_web/controllers/users_controller.ex
@@ -0,0 +1,23 @@
+defmodule BananaBankWeb.UsersController do
+  use BananaBankWeb, :controller
+
+  alias BananaBank.Users.Create
+  alias BananaBank.Users.User
+
+  action_fallback BananaBankWeb.FallbackController
+
+  def create(conn, params) do
+    with {:ok, %User{} = user} <- Create.call(params) do
+      conn
+        |> put_status(:created)
+        |> render("create.json", user: user)
+    end
+  end
+
+  # defp handle_response({:error, changeset}, conn) do
+  #   conn
+  #   |> put_status(:bad_request)
+  #   |> put_view(json: BananaBankWeb.ErrorJSON)
+  #   |> render(:error, changeset: changeset)
+  # end
+end
