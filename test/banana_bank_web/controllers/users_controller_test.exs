603e5ac875805dae6c56a8de9711fe8736c50ad0
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-08 22:23:03 -0300

    test success delete

--- test/banana_bank_web/controllers/users_controller_test.exs
+++ test/banana_bank_web/controllers/users_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule BananaBankWeb.UsersControllerTest do
   use BananaBankWeb.ConnCase
 
+  alias BananaBank.Users
+
   describe "create/2" do
     test "successfully creates an user", %{conn: conn} do
       params = %{
@@ -46,6 +48,34 @@ defmodule BananaBankWeb.UsersControllerTest do
                  "password" => ["should be at least 6 character(s)"],
                  "zip_code" => ["should be 8 character(s)"]
                }
+             } = response
+    end
+  end
+
+  describe "delete/2" do
+    test "successfully deletes an user", %{conn: conn} do
+      params = %{
+        name: "John Doe",
+        email: "jhon@email.com",
+        password: "123456",
+        zip_code: "12345678"
+      }
+
+      {:ok, user} = Users.create(params)
+
+      response =
+        conn
+        |> delete(~p"/api/users/#{user.id}")
+        |> json_response(:ok)
+
+      assert %{
+               "data" => %{
+                 "email" => "jhon@email.com",
+                 "id" => _id,
+                 "name" => "John Doe",
+                 "zip_code" => "12345678"
+               },
+               "message" => "user deleted successfully"
              } = response
     end
   end
