28bb045251f99cc03c52dd9247eca0a2ec0f32af
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-10 16:51:04 -0300

    test "successfully returns zip code info"

--- lib/banana_bank/via_cep/client.ex
+++ lib/banana_bank/via_cep/client.ex
@@ -1,11 +1,11 @@
 defmodule BananaBank.ViaCep.Client do
   use Tesla
 
-  plug Tesla.Middleware.BaseUrl, "https://viacep.com.br/ws"
+  @default_url "https://viacep.com.br/ws"
   plug Tesla.Middleware.JSON
 
-  def call(zip_code) do
-    "#{zip_code}/json"
+  def call(url \\ @default_url, zip_code) do
+    "#{url}/#{zip_code}/json"
     |> get()
     |> handle_response()
   end
