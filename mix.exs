11f81c6e02e481d8b44b25852451883d6bfa3e93
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-10 14:20:20 -0300

    format; remove unused tesla adapter

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule BananaBank.MixProject do
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:argon2_elixir, "~> 4.0"},
-       {:tesla, "~> 1.11"}
+      {:tesla, "~> 1.11"}
     ]
   end
 
