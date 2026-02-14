51420ac4f52b77a5b5cf7239c0260c0a0375872c
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-14 18:27:29 -0300

    example of tasks usage

---
+++ lib/banana_bank/tasks.ex
@@ -0,0 +1,27 @@
+defmodule BananaBank.Tasks do
+
+  def call do
+    # Task.start(&heavy_work/0)
+    # light_work()
+
+    task = Task.async(&heavy_work/0)
+    light_work()
+    light_work()
+    light_work()
+    light_work()
+    light_work()
+    light_work()
+    light_work()
+    Task.await(task)
+  end
+
+  defp heavy_work do
+    :timer.sleep(5000)
+    IO.inspect("Heavy Work")
+  end
+
+  defp light_work do
+    :timer.sleep(1000)
+    IO.inspect("Light Work")
+  end
+end
