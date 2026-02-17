bfcbb0dd8482daae56cb789faf7514bcf80c8c83
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-17 12:53:11 -0300

    allow fallback controller handle with generic messages

--- lib/banana_bank_web/controllers/fallback_controller.ex
+++ lib/banana_bank_web/controllers/fallback_controller.ex
@@ -22,10 +22,17 @@ defmodule BananaBankWeb.FallbackController do
     |> render(:error, status: :internal_server_error)
   end
 
-  def call(conn, {:error, changeset}) do
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
     conn
     |> put_status(:bad_request)
     |> put_view(json: BananaBankWeb.ErrorJSON)
     |> render(:error, changeset: changeset)
+  end
+
+  def call(conn, {:error, reason}) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(json: BananaBankWeb.ErrorJSON)
+    |> render(:error, status: reason)
   end
 end
