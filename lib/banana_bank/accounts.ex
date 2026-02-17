9bbe737bcc517716ca0f728e658b5d4f83712ec2
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-17 12:08:50 -0300

    add transaction to accounts facade

--- lib/banana_bank/accounts.ex
+++ lib/banana_bank/accounts.ex
@@ -1,6 +1,8 @@
 defmodule BananaBank.Accounts do
   alias BananaBank.Accounts.{Create, Get}
+  alias BananaBank.Accounts.Transaction
 
   defdelegate create(params), to: Create, as: :call
   defdelegate get(id), to: Get, as: :call
+  defdelegate transaction(from_account_id, to_account_id, value), to: Transaction, as: :call
 end
