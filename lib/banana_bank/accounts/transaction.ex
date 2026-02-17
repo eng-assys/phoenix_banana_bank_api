d81732153b613d8be20ffd874ea453c7d1261783
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-17 12:04:01 -0300

    handle equal accoount id error; handle invalid transaction value error

--- lib/banana_bank/accounts/transaction.ex
+++ lib/banana_bank/accounts/transaction.ex
@@ -6,14 +6,18 @@ defmodule BananaBank.Accounts.Transaction do
   alias Ecto.Multi
 
   def call(from_account_id, to_account_id, value) do
-    with {:ok, %Account{} = from_account}  <- Accounts.get(from_account_id),
-         {:ok, %Account{} = to_account} <-  Accounts.get(to_account_id) do
+    with {:ok, :different_account_id} <- equal_account_ids(from_account_id, to_account_id),
+         {:ok, %Account{} = from_account} <- Accounts.get(from_account_id),
+         {:ok, %Account{} = to_account} <- Accounts.get(to_account_id),
+         {:ok, value} <- Decimal.cast(value) do
       Multi.new()
       |> withdraw(from_account, value)
       |> deposit(to_account, value)
       |> Repo.transact()
     else
+      {:error, :equal_account_ids} -> {:error, :equal_account_ids}
       {:error, :not_found} -> {:error, :not_found}
+      :error -> {:error, "invalid value for transaction"}
     end
   end
 
@@ -22,6 +26,13 @@ defmodule BananaBank.Accounts.Transaction do
     changeset = Account.changeset(to_account, %{balance: new_balance})
     Multi.update(multi, :deposit, changeset)
   end
+
+  defp equal_account_ids(from_account_id, to_account_id) do
+  case from_account_id == to_account_id do
+    true -> {:error, :equal_account_ids}
+    false -> {:ok, :different_account_id}
+  end
+end
 
   defp withdraw(multi, from_account, value) do
     new_balance = Decimal.sub(from_account.balance, value)
